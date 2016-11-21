e243869aa8cca04b016c97408aeb82994220a06c
mohamed gassama <bouyagassama@gmail.cpm>
2016-11-21 16:26:49 -0500

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 require('dotenv').config({ silent: true });
 const express     = require('express');
 const logger      = require('morgan');
 const path        = require('path');
 const bodyParser  = require('body-parser');
-const puppyRouter = require('./routes/api/puppies');
+
 
 const app         = express();
 const PORT        = process.argv[2] || process.env.port || 3000;
@@ -16,6 +16,6 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // This will parse our payload from fetch which is sent as a JSON object
 app.use(bodyParser.json());
 
-app.use('/api/puppies', puppyRouter);
+
 
 app.listen(PORT, () => console.log('server here! listening on', PORT));

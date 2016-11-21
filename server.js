b07557743222163347b266e055f3ccb0f5ab90e8
Alexander Tong <tongsalex@gmail.com>
2016-11-21 15:05:46 -0500

    build template for all group members

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict'
+require('dotenv').config({ silent: true });
+const express     = require('express');
+const logger      = require('morgan');
+const path        = require('path');
+const bodyParser  = require('body-parser');
+const puppyRouter = require('./routes/api/puppies');
+
+const app         = express();
+const PORT        = process.argv[2] || process.env.port || 3000;
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// This will parse our payload from fetch which is sent as a JSON object
+app.use(bodyParser.json());
+
+app.use('/api/puppies', puppyRouter);
+
+app.listen(PORT, () => console.log('server here! listening on', PORT));

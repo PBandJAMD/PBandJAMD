8c2c1d6beb7d4c7604f91c90c15c406b56c1a302
Jaemin Han <jaemin.han@nyu.edu>
2016-11-23 10:58:40 -0500

    sidebar working but need to add functionality

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,15 +1,61 @@
-import React, { Component } from 'react';
-import from './Aside.css';
-import LoginForm from '../LoginForm/LoginForm';
-import SignupForm from '../SignupForm/SignupForm';
+import React from 'react';
+import './Aside.css';
 
-export default class Aside extends Component {
+ // function created for 'enter' key
+// function handleKeyPress(e, cb) {
+//   if (e.key === 'Enter') cb();
+// }.
 
-  render() {
-    return(
-      <div>
+const Aside = props => (
+  <div id="user" className="toggle">
+    <a href="#user" className="sign-button">Log In | Sign Up</a>
 
+    <div id="log-in" className="both">
+      <h3>- Login In -</h3>
+      <div className="label">
+        <label>User ID:</label>
+        <input
+          type="text"
+        />
       </div>
-    )
-  }
-}
+
+      <div className="label">
+        <label>Password:</label>
+        <input
+          type="password"
+        />
+      </div>
+
+      <div className="label button-position">
+        <button className="bt click">Sign In</button>
+      </div>
+
+      <br />
+      <br />
+      <br />
+
+      <div id="sign-up" className="both">
+        <h3>- Sign Up Today! -</h3>
+        <div className="label">
+          <label>User ID:</label>
+          <input
+            type="text"
+          />
+        </div>
+        <div className="label">
+          <label>Password:</label>
+          <input
+            type="password"
+          />
+        </div>
+        <div className="label button-position">
+          <button className="bt click">Sign Up</button>
+        </div>
+        <a href="" className="bt">Cancel</a>
+      </div>
+    </div>
+
+  </div>
+);
+
+export default Aside;

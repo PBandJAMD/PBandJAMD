1707f75b07438f582bcc56d4e2af79bff92fa4b6
Jaemin Han <jaemin.han@nyu.edu>
2016-11-24 16:08:05 -0500

    toggle functionality in process

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -39,54 +39,76 @@ import style from './Aside.css';
 // }
 
 const Aside = props => (
-  <div id="user" className="toggle">
-    {/*<div id="myBtn" className="sign-button toggle">Log In | Sign Up</div>*/}
-      <a href="#user" className="sign-button">Log In | Sign Up</a>
+  <div id="/" className="toggle">
+   <a href="#/" className="sign-button">Log In | Sign Up</a>
+{/*<a href="#user" className="sign-button">Log In | Sign Up</a>*/}
+{/*<div id="myBtn" className="sign-button toggle">Log In | Sign Up</div>*/}
 
-
+    {/*BEGIN returned user input fields*/}
     <div id="log-in" className="both">
       <h3>- Login In -</h3>
       <div className="label">
-        <label>User ID:</label>
+        <label>User ID: </label>
         <input
           type="text"
-
+          // value=""
+          // OnChange=""
+          // onKeyPress=""
+          // placeholder=""
         />
       </div>
 
       <div className="label">
-        <label>Password:</label>
+        <label>Password: </label>
         <input
           type="password"
+          // value=""
+          // OnChange=""
+          // onKeyPress=""
+          // placeholder=""
         />
       </div>
 
+    {/*This button is for returning user*/}
       <div className="label button-position">
         <button className="bt click">Sign In</button>
+        {/*OnClick={} inside <button>*/}
       </div>
+    {/*END returned user*/}
 
       <br />
       <br />
-      <br />
 
+    {/*BEGIN new user input fields*/}
       <div id="sign-up" className="both">
         <h3>- Sign Up Today! -</h3>
         <div className="label">
-          <label>User ID:</label>
+          <label>User ID: </label>
           <input
             type="text"
-
+          // value=""
+          // OnChange=""
+          // onKeyPress=""
+          // placeholder=""
           />
         </div>
+
         <div className="label">
-          <label>Password:</label>
+          <label>Password: </label>
           <input
             type="password"
+          // value=""
+          // OnChange=""
+          // onKeyPress=""
+          // placeholder=""
           />
         </div>
         <div className="label button-position">
           <button className="bt click">Sign Up</button>
+          {/*OnClick={} inside <button>*/}
         </div>
+      {/*END new user input fields*/}
+
         <a href="" className="bt">Cancel</a>
       </div>
     </div>

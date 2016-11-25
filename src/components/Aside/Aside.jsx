b3453bb1cd5e776d3e63d258494003d45c9ac79a
Jaemin Han <jaemin.han@nyu.edu>
2016-11-24 21:17:31 -0500

    functional sidebar

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import '../normalize.css';
 import './Aside.css';
 import LoginForm from '../LoginForm/LoginForm.jsx';
+import SignupForm from '../SignupForm/SignupForm.jsx';
 
 const Aside = props => (
 
-
   <div id={props.sidebar}>
-    <LoginForm
-    handleUsernameInput={props.handleUsernameInput}
-    handlePasswordInput={props.handlePasswordInput}
-    handleLogin={props.handleLogin}
-    />
 
-    <SignupForm
+    <div id="sidebar">
 
+      <LoginForm
+        handleUsernameInput={props.handleUsernameInput}
+        handlePasswordInput={props.handlePasswordInput}
+        handleLogin={props.handleLogin}
+      />
 
-    />
+      <SignupForm
+        handleUsernameInput={props.handleUsernameInput}
+        handlePasswordInput={props.handlePasswordInput}
+        handleSignup={props.handleSignup}
+      />
+    </div>
 
   </div>
     );

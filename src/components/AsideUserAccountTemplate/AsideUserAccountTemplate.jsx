209dc2d93bf5b0e21c700ad4c4642d7473df1aef
Alexander Tong <tongsalex@gmail.com>
2016-11-27 17:56:43 -0500

    css

--- src/components/AsideUserAccountTemplate/AsideUserAccountTemplate.jsx
+++ src/components/AsideUserAccountTemplate/AsideUserAccountTemplate.jsx
@@ -4,14 +4,13 @@ import './AsideUserAccountTemplate.css';
 
 const AsideUserAccountTemplate = props => (
   <div id="aside-user-account-template">
-      <h1 id="commentTitle">{props.title}</h1>
-      <br />
-      <p>{props.content}</p>
-      <br />
-      <p>Created on: {props.date_created}</p>
 
+      <h1 id="commentTitle">{props.title}</h1>
+      <p>{props.content}</p>
+      <p>Created on: {props.date_created}</p>
       <button>Delete</button>
       <button>Edit</button>
+
   </div>
 );
 

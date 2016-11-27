e73960e59ef4a497dfe6d2bf7c9b2a0baf9350ba
Alexander Tong <tongsalex@gmail.com>
2016-11-27 16:33:01 -0500

    fix css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = props => (
   <div id="footer-container">
     <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch" rel="stylesheet" />
-    <h3>Made with &#9829; by JAMD 2016.</h3>
+    <h3 id="footerText">Made with &#9829; by JAMD 2016.</h3>
   </div>
 );
 

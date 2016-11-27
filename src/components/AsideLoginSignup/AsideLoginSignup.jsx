48b44813f672fab4a8435f85878ccd1627a4d570
Alexander Tong <tongsalex@gmail.com>
2016-11-27 13:54:57 -0500

    add sidebar button

--- src/components/AsideLoginSignup/AsideLoginSignup.jsx
+++ src/components/AsideLoginSignup/AsideLoginSignup.jsx
@@ -5,23 +5,29 @@ import LoginForm from '../LoginForm/LoginForm.jsx';
 import SignupForm from '../SignupForm/SignupForm.jsx';
 
 const AsideLoginSignup = props => (
+  <div id="AsideLoginSignupContainer">
+    <div id="sidebarButton">
+      <p id="sidebarButtonText">Hide Login/Signup</p>
+    </div>
 
-  <div id={props.sidebar}>
-      <SignupForm
-        signupUsername={props.signupUsername}
-        updateSignupFormUsername={props.updateSignupFormUsername}
-        signupPassword={props.signupPassword}
-        updateSignupFormPassword={props.updateSignupFormPassword}
-        handleSignupFormSubmit={props.handleSignupFormSubmit}
-      />
+    <div id={props.sidebar}>
+        <SignupForm
+          signupUsername={props.signupUsername}
+          updateSignupFormUsername={props.updateSignupFormUsername}
+          signupPassword={props.signupPassword}
+          updateSignupFormPassword={props.updateSignupFormPassword}
+          handleSignupFormSubmit={props.handleSignupFormSubmit}
+        />
 
-      <LoginForm
-        loginUsername={props.loginUsername}
-        updateLoginFormUsername={props.updateLoginFormUsername}
-        loginPassword={props.loginPassword}
-        updateLoginFormPassword={props.updateLoginFormPassword}
-        handleLoginFormSubmit={props.handleLoginFormSubmit}
-      />
+        <LoginForm
+          loginUsername={props.loginUsername}
+          updateLoginFormUsername={props.updateLoginFormUsername}
+          loginPassword={props.loginPassword}
+          updateLoginFormPassword={props.updateLoginFormPassword}
+          handleLoginFormSubmit={props.handleLoginFormSubmit}
+        />
+    </div>
+
   </div>
     );
 

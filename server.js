34a17c10299588970f8ccd2b92fcbaa6975b03fd
Alexander Tong <tongsalex@gmail.com>
2016-11-28 18:15:55 -0500

    change port to PORT

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const userRoute = require('./routes/api/user.js');
 const authRoute = require('./routes/api/auth.js');
 
 const app = express();
-const PORT = process.argv[2] || process.env.port || 3000;
+const PORT = process.argv[2] || process.env.PORT || 3000;
 
 app.use(logger('dev'));
 
